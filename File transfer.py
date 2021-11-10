4145934ab93c475b10aefd653c8f13ec15e502c1
ericrymer <90289727+ericrymer@users.noreply.github.com>
2021-11-10 09:41:41 -0800

    Update File transfer.py

--- File transfer.py
+++ File transfer.py
@@ -5,10 +5,10 @@ import time
 SECONDS_IN_DAY = 24 * 60*60 
 
 #set where the source of the files are
-source = 'C:/Users/ericr/Desktop/Folder A/'
+src = "C:\Users\Student\Desktop\FolderA"
 
 #set the destinaton path to folder b
-destination =  'C:/Users/ericr/Desktop/Folder B/'
+dst = "C:\Users\Student\Desktop\FolderB"
 
 now = time.time()
 before = now - SECONDS_IN_DAY
@@ -18,9 +18,9 @@ def last_mod_time(fname):
     return os.path.getmtime(fname)
 
 
-for fname in os.listdir(source):
+for fname in os.listdir(src):
     src_fname = os.path.join(src, fname)
-    if last_mod_time(source_fname) > before:
+    if last_mod_time(src_fname) > before:
         dst_fname = os.path.join(dst, fname)
         shutil.move(src_fname, dst_fname)
 
